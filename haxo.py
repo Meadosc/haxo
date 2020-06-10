6d24c55e1fad6e7ae611a3411087accb7c7813c6
rahulunair@gmail.com <rahulunair@gmail.com>
2020-06-10 01:47:39 -0700

    command line stub

--- haxo.py
+++ haxo.py
@@ -1,11 +1,16 @@
 """entry point script."""
 import click
+import pandas as pd
 import tabulate
 
-import pkg_info
+from pkg_info import apt_pkgs
+from pkg_info import apt_licenses
+from pkg_info import npm_pkgs
+from pkg_info import pip_pkgs
+from pkg_info import rpm_pkgs
 
 
-@cli.group()
+@click.group()
 def cli():
     """Haxo - look inside a docker image.
 
@@ -13,3 +18,29 @@ def cli():
     installed in a container image using package managers such
     as apt, dnf, rpm, pip and npm.
     """
+
+
+@cli.command("pip")
+@click.argument("image", type=str)
+@click.option("--format", default="csv", type=str)
+@click.option("--license", default="spdx", type=str)
+@click.option("--show/--no-show", default=False)
+def pip(image, format="csv", license="spdx", show=False):
+    """pip package info.
+    
+    Creates a csv file and saves it to the data directory
+    by default with name, verson and license info 
+    of python packages in the given image.
+    """
+    fname = "data/pip-pkgs-{}.{}".format(image, format)
+    # concurrent futures
+    # cache file based on image sha in the pkg_utils script
+    # pkgs = pip_pkgs(image)
+    # print(pkgs, file=open(fname, "w"))
+    headers = ["Package", "Version", "License"]
+    if show:
+        print(tabulate.tabulate(pd.read_csv(fname), headers=headers))
+
+
+if __name__ == "__main__":
+    cli()
