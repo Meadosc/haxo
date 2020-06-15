27b2b08fb994f54d91011e922e0e74c2f3d91282
Rahul Nair <rahulunair@users.noreply.github.com>
2020-06-15 12:15:11 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 project = "haxo"
 author = "rahul"
+master_doc = 'index'
 copyright = f"2020, {author}"
 copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
