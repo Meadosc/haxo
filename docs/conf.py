2d56b66edc4f37b28f8106ac8b2a034d791796e1
Rahul Nair <rahulunair@users.noreply.github.com>
2020-06-15 01:48:01 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,4 +2,6 @@
 project = "haxo"
 author = "rahul"
 copyright = f"2020, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
+extensions = ["sphinx.ext.autodoc",
+ "sphinx.ext.napoleon", 
+"sphinx-autodoc-typehints"]
