e364c8fae6990c7887895d7fb54572dc25552f26
cord <cord.meados@intel.com>
2020-06-23 09:47:08 -0700

    update test_diff.py

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -8,7 +8,7 @@ from haxo import diff
 def test_diff():
     d_old = {'Name': ['test1', 'test2'], 'Version': ['1.2', '3.4'], 'License': ['BSD', 'GCL']}
     d_new = {'Name': ['test1', 'test3'], 'Version': ['1.2', '2.1'], 'License': ['BSD', 'BSD']}
-    diff_added, diff_removed = diff('./test1.csv','./test2.csv')
+    diff_added, diff_removed = diff('./test1.csv','./test2.csv', test=True)
     d_added = {'Name': ['test3'], 'Version': ['2.1'], 'License': ['BSD']}
     d_remov = {'Name': ['test2'], 'Version': ['3.4'], 'License': ['GCL']}
     assert d_added == diff_added
